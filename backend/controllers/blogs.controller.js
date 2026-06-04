8030c216ad3cd8352abc5707a6175f015fe13dd8
SimiyuVic <charlesvictor369@gmail.com>
2026-06-04 16:55:24 +0300

    Edit and delete blogs api

--- backend/controllers/blogs.controller.js
+++ backend/controllers/blogs.controller.js
@@ -82,27 +82,77 @@ export const blogContent = async(req, res)=>{
     }
 }
 
+//edit blog
+export const editBlog = async(req, res)=>{
+  try 
+  {
+    const { id } = req.params;
+    const { blogTitle, blogDescription, blogContent } = req.body;
+
+    if(!blogTitle || !blogDescription || !blogContent)
+    {
+      return res.status(400).json({
+        success: false,
+        message: "All Fields Required before an Update!"
+      });
+    }
+
+    const blog = await Blog.findByIdAndUpdate(id, 
+      {
+        blogTitle, 
+        blogDescription, 
+        blogContent
+      }
+    );
+
+    if(!blog)
+    {
+      return res.status(404).json({
+        success: false,
+        message: "Blog Does Not Exist!"
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Blog Updated !"
+    });
+  } 
+  catch (error) 
+  {
+    return res.status(500).json({
+      success: false,
+      message: "Cannot Edit Blog!"
+    });
+  }
+}
+
 //delete blog
 export const deleteBlog = async(req, res)=>{
-    try {
-        const { id } = req.params;
-        const blog = await Blog.findByIdAndDelete(id);
-        if(!blog)
-        {
-            return res.status(404).json({
-            success: false,
-            message: "Blog Does Not Exist!"
-        });
-        return res.status(200).json({
-            success: true,
-            message: "Blog Deleted!"
-        });
-        }
+  try 
+  {
+    const { id } = req.params;
 
-    } catch (error) {
-        return res.status(500).json({
-            success: false,
-            message: "Cannot Delete Blog!"
-        });
+    const blog = await Blog.findByIdAndDelete(id)
+
+    if(!blog)
+    {
+      return res.status(404).json({
+        success: false,
+        message: "Blog Does Not Exist!"
+      });
     }
+
+    return res.status(200).json({
+      success: true,
+      message: "Blog Deleted !"
+    });
+  } 
+  catch (error) 
+  {
+    return res.status(500).json({
+      success: false,
+      message: "Cannot Delete Blog!"
+    });
+  }
 }
